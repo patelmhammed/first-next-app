d72d7492411e9481120d85e1cf195d8aca87f3d0
patelmhammed <muhammedpatel538@gmail.com>
2024-03-09 22:35:05 +0530

    me page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,6 +6,7 @@ import toast from "react-hot-toast";
 
 export default function profilePage() {
   const router = useRouter();
+
   const onLogout = async () => {
     try {
       const response = await axios.get("/api/users/logout");
@@ -17,6 +18,13 @@ export default function profilePage() {
       toast.error(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    const response = await axios.get("/api/users/me");
+    const id = response.data.data._id;
+    router.push(`/profile/${id}`);
+  };
+
   return (
     <div
       className="flex flex-col items-center
@@ -30,6 +38,13 @@ export default function profilePage() {
         className="bg-blue-500 mt-4 hover:bg-blue-700 
       text-white font-bold py-2 px-4 rounded">
         Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 
+        text-white font-bold py-2 px-4 rounded">
+        My Details
       </button>
     </div>
   );
