90ed629837f48faeddf6f6e76bb65f2458e15142
patelmhammed <muhammedpatel538@gmail.com>
2024-03-09 19:36:29 +0530

    logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 export default function profilePage() {
+  const router = useRouter();
+  const onLogout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      console.log(response);
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
   return (
     <div
       className="flex flex-col items-center
@@ -6,6 +24,13 @@ export default function profilePage() {
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <hr />
+      <button
+        onClick={onLogout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 
+      text-white font-bold py-2 px-4 rounded">
+        Logout
+      </button>
     </div>
   );
 }
