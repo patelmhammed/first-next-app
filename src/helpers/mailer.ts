ce305b619420e83b8169dfdfe40317761f474905
patelmhammed <muhammedpatel538@gmail.com>
2024-03-10 19:42:47 +0530

    mailer added

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,63 @@
+// domain.com/verifytoken/adadscsvvvvreece - when doing from server side
+// domain.com/verifytoken?token=adadscsvvvvreece - when doing from client side
+
+import nodemailer from "nodemailer";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+
+export const sendEMail = async ({
+  email,
+  emailType,
+  userId,
+}: any) => {
+  try {
+    const hashedToken = await bcryptjs.hash(
+      userId.toString(),
+      10
+    );
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transport = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject:
+        emailType === "VERIFY"
+          ? "Verify your email"
+          : "Reset your password",
+      html: `<p>Click <a href="${process.env.DOMAIN}/
+      verifyemail?token=${hashedToken}">here</a> to 
+      ${
+        emailType === "VERIFY"
+          ? "Verify your email"
+          : "Reset your password"
+      }</p>`,
+    };
+
+    const mailResponse = await transport.sendMail(
+      mailOptions
+    );
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
